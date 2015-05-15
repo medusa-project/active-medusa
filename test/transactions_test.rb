9c82b87f4a9c60e47bde1e5a2e0566472e77d103
Alex Dolski <alexd@n-ary.net>
2015-05-15 13:02:46 -0500

    Add TransactionsTest

---
+++ test/transactions_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class TransactionsTest < Minitest::Test
+
+  class Foo
+    include ActiveMedusa::Transactions
+    attr_accessor :transaction_url
+  end
+
+  def setup
+    @f4_url = ActiveMedusa::Configuration.instance.fedora_url.chomp('/')
+    @obj = Foo.new
+    @obj.transaction_url = @f4_url + '/tx:23942034982340'
+    @non_tx_url = @f4_url + '/bla/bla/bla'
+    @tx_url = @obj.transaction_url + '/bla/bla/bla'
+  end
+
+  def test_nontransactional_url_should_work_properly
+    assert_equal @non_tx_url, @obj.nontransactional_url(@tx_url)
+  end
+
+  def test_transactional_url_should_work_properly
+    assert_equal @tx_url, @obj.transactional_url(@non_tx_url)
+
+    # don't transactionalize an already-transactional url
+    assert_equal @tx_url, @obj.transactional_url(@tx_url)
+  end
+
+end
