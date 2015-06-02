04f2a34d9bfe33cbb0f25f44219571a0716a5f49
Alex Dolski <alexd@n-ary.net>
2015-06-02 09:46:14 -0500

    Add an assertion to test_client

--- test/fedora_test.rb
+++ test/fedora_test.rb
@@ -5,6 +5,9 @@ class FedoraTest < Minitest::Test
   def test_client
     client = ActiveMedusa::Fedora.client
     assert_instance_of HTTPClient, client
+
+    response = client.get(ActiveMedusa::Configuration.instance.fedora_url)
+    assert_equal 200, response.status
   end
 
 end
