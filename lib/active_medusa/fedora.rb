25b88c55ca6911f822b1107fc0d9b095b8c9f99f
Alex Dolski <alexd@n-ary.net>
2015-06-02 09:57:54 -0500

    Remove MANAGED_PREDICATES

--- lib/active_medusa/fedora.rb
+++ lib/active_medusa/fedora.rb
@@ -6,17 +6,6 @@ module ActiveMedusa
 
     @@http_client = nil
 
-    # Assume that predicate URIs that start with any of these are
-    # repository-managed. This may not be a safe assumption, but it works for
-    # us for now.
-    MANAGED_PREDICATES = [
-        'http://fedora.info/definitions/',
-        'http://www.jcp.org/jcr',
-        'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
-        'http://www.w3.org/2000/01/rdf-schema#',
-        'http://www.w3.org/ns/ldp#'
-    ]
-
     ##
     # @return [HTTPClient]
     #
