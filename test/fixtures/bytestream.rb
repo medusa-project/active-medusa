5921b4e28737b92734d1dab02ea9cdc022fc4113
Alex Dolski <alexd@n-ary.net>
2015-06-18 14:05:53 -0500

    Fix belongs_to statement

--- test/fixtures/bytestream.rb
+++ test/fixtures/bytestream.rb
@@ -2,6 +2,7 @@ class Bytestream < ActiveMedusa::Binary
 
   entity_class_uri 'http://example.org/Bytestream'
 
-  belongs_to :item, predicate: 'http://example.org/isOwnedBy'
+  belongs_to :item, predicate: 'http://example.org/isOwnedBy',
+             solr_field: 'item'
 
 end
