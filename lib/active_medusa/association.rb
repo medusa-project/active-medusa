11a8382cb88d9d38dca605e0ea414f3cc9a2b511
Alex Dolski <alexd@n-ary.net>
2015-07-10 16:48:19 -0500

    Document the Association attributes

--- lib/active_medusa/association.rb
+++ lib/active_medusa/association.rb
@@ -7,11 +7,36 @@ module ActiveMedusa
       HAS_MANY = :has_many
     end
 
+    # @!attribute cascade_deletes
+    #   @return [Boolean] Applicable only to has-many associations.
+    attr_accessor :cascade_deletes
+
+    # @!attribute name
+    #   @return [String] The name of the association's accessor on the source
+    #   (declaring) side.
     attr_accessor :name
+
+    # @!attribute rdf_predicate
+    #   @return [String] The RDF predicate to use to store the association in
+    #   Fedora.
     attr_accessor :rdf_predicate
+
+    # @!attribute solr_field
+    #   @return [String, Symbol] The Solr field in which the value of the
+    #   `rdf_predicate` triple is stored.
     attr_accessor :solr_field
+
+    # @!attribute source_class
+    #   @return [String] The class of the source (declaring) side of the
+    #   association.
     attr_accessor :source_class
+
+    # @!attribute target_class
+    #   @return [String] The class of the target side of the association.
     attr_accessor :target_class
+
+    # @!attribute type
+    #   @return [ActiveMedusa::Association::Type]
     attr_accessor :type
 
     ##
