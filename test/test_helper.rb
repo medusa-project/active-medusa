5d11fd3ed344208e558a13b00f50be43bb871d68
Alex Dolski <alexd@n-ary.net>
2015-08-19 15:04:13 -0500

    Use a different class predicate

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,7 +10,7 @@ autoload :Seeder, './fixtures/seeder'
 ActiveMedusa::Configuration.new do |config|
   config.fedora_url = 'http://localhost:8080/fedora/rest'
   config.fedora_indexing_transformation = 'activemedusatest'
-  config.class_predicate = 'http://example.org/hasClass'
+  config.class_predicate = 'http://www.w3.org/2000/01/rdf-schema#Class'
   config.solr_url = 'http://localhost:8983/solr'
   config.solr_core = 'activemedusa'
   config.solr_more_like_this_endpoint = '/mlt'
